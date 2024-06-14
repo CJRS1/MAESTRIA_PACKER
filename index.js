e19752cd90246f52380da8e856c1b60a599517dc
CJRS1 <reyes.christian@pucp.pe>
2024-06-14 16:31:42 -0500

    Se hicieron los últimos cambios

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
 const server = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.end('<h1 style="color: green;"><i>Hello, World - Christian Reyes</i></h1>');
 });
-server.listen(3000);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
